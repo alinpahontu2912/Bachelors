44bb696dddef2e518d0ff9b3bd75706180cba0ca
alinpahont2912 <alin_pahontu@yahoo.com>
2023-04-15 16:05:10 +0300

    add router

--- src/pages/TestPage.vue
+++ src/pages/TestPage.vue
@@ -1,97 +1,20 @@
 <template>
-  <div class="rowContainer row space-around">
-    <div class="col-7 row q-px-md">
-      <q-card class="row" style="height: 600px">
-        <q-card-section class="rowContainer col-12">
-          <div class="text-h4 text-bold ">{{ $t('signUp_title') }}</div>
-        </q-card-section>
-        <q-card-section class="rowContainer col-12">
-          <q-form class="columnContainer item-medium row col-12">
-            <q-input standout="bg-secondary text-white" rounded type="email" :label="$t('email')"
-              class="q-pa-md col-10 item-medium">
-              <template v-slot:prepend>
-                <q-icon name="mail" />
-              </template>
-              <template v-slot:append>
-                <!-- @click="text = '' -->
-                <q-icon name="close" class="cursor-pointer" />
-              </template>
-            </q-input>
-            <q-input standout="bg-secondary text-white" rounded :type="isPwd ? 'password' : 'text'"
-              :label="$t('password')" class="q-pa-md col-10 item-medium">
-              <template v-slot:prepend>
-                <q-icon name="lock" />
-              </template>
-              <template v-slot:append>
-                <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer" @click="isPwd = !isPwd" />
-              </template>
-            </q-input>
-            <q-select rounded class="q-pa-md col-10 item-medium" standout="bg-teal text-white" v-model="model"
-              :options="options" :label="$t('user_job')">
-              <template v-slot:prepend>
-                <q-icon name="settings_accessibility" />
-              </template>
-            </q-select>
-          </q-form>
-        </q-card-section>
-        <q-card-section class="rowContainer col-10">
-          <q-form class="rowContainer item-medium col-12">
-            <q-checkbox class="q-pa-md col-4 item-medium" left-label v-model="isPwd" :label="$t('remember_me')"
-              checked-icon="task_alt" unchecked-icon="highlight_off" />
-            <q-btn rounded outlined unelevated size="md" class="q-pa-md col-4 item-medium bg-secondary text-white"
-              :label="$t('create_account')" />
-          </q-form>
-        </q-card-section>
-      </q-card>
-    </div>
-    <div class="col-5 row q-px-md">
-      <q-card class="row bg-secondary justify-center text-white" style="height: 600px">
-        <q-card-section class="rowContainer col-12">
-          <div class="text-h4 text-bold" style="text-align: center;">{{ $t('already_have_account') }}</div>
-        </q-card-section>
-        <q-card-section class="rowContainer col-12">
-          <div class="text-h3 text-white text-center q-pa-xl">{{ $t('returning_description') }}</div>
-        </q-card-section>
-        <q-card-section class="rowContainer col-8">
-          <q-form class="row content-center justify-center align-center item-medium col-8">
-            <q-btn rounded outlined unelevated size="md" class="q-pa-md col-12 item-medium bg-white text-black"
-              :label="$t('go_to_login')" />
-          </q-form>
-        </q-card-section>
-      </q-card>
-    </div>
-  </div>
+  <q-btn @click="testRouteLogin">
+    Test Login
+
+  </q-btn>
+  <q-btn>
+    Test SignUp
+  </q-btn>
 </template>
 <script setup>
-import { ref } from 'vue'
-const isPwd = ref(true)
-const lorem = ref('')
+import { useRouter } from 'vue-router';
 
+const router = useRouter()
+
+function testRouteLogin() {
+  console.log(router)
+  router.push('/announcements')
+}
 </script>
-<style>
-.columnContainer {
-  display: flex;
-  flex-wrap: nowrap;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
 
-.rowContainer {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: center;
-  align-content: center;
-}
-
-.item-small {
-  flex-grow: 1;
-}
-
-.item-medium {
-  flex-grow: 3;
-
-}
-</style>
